4174296f0b526a6f50b5bfcec3b14e1c41d73a8b
Luis Menina <liberforce@freeside.fr>
2025-12-02 20:01:43 +0100

    fix: fix basic shell

--- day2/puzzle1/find_sum_of_bad_ids.py
+++ day2/puzzle1/find_sum_of_bad_ids.py
@@ -1,18 +1,30 @@
+import logging
+
+LOGGER = logging.getLogger(__name__)
+
+
 def get_ranges(str_range) -> list[tuple[int, int]]:
-    return []
+    ranges = str_range.strip().split(",")
+    logging.debug("Ranges: %s", ranges)
+    return ranges
 
 
-def check_ids(ranges: list[tuple[int, int]]) -> int:
+def check_ids(ranges: list[tuple[int, int]]) -> list[int]:
     invalid_ids: list[int] = []
-    return sum(invalid_ids)
+    return invalid_ids
 
 
 def get_sum_of_invalid_ids() -> int:
     with open("data.txt", "r") as fp:
-        ranges = get_ranges(fp.read().split(","))
-        result = check_ids(ranges)
-        return result
+        ranges = get_ranges(fp.read())
+        invalid_ids = check_ids(ranges)
+        return sum(invalid_ids)
 
 
 def main():
+    logging.basicConfig(level=logging.DEBUG)
     print(get_sum_of_invalid_ids())
+
+
+if __name__ == "__main__":
+    main()
