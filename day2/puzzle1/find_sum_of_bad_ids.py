69883f578b043b8dbeaff41c60e7669df72e5735
Luis Menina <liberforce@freeside.fr>
2025-12-02 22:05:32 +0100

    feat: add range traversing logic

--- day2/puzzle1/find_sum_of_bad_ids.py
+++ day2/puzzle1/find_sum_of_bad_ids.py
@@ -3,21 +3,42 @@ import logging
 LOGGER = logging.getLogger(__name__)
 
 
-def get_ranges(str_range) -> list[tuple[int, int]]:
-    ranges = str_range.strip().split(",")
-    logging.debug("Ranges: %s", ranges)
+def get_ranges(str_range: str) -> list[range]:
+    ranges: list[range] = []
+    range_strings = str_range.strip().split(",")
+    for range_string in range_strings:
+        bounds = [int(v) for v in range_string.split("-")]
+        ranges.append(range(bounds[0], bounds[1] + 1))
+
     return ranges
 
 
-def check_ids(ranges: list[tuple[int, int]]) -> list[int]:
+def is_valid_id(id_: int):
+    str_id = str(id)
+    if len(str_id) % 2 == 1:
+        return False
+
+    return True
+
+
+def check_ids(ranges: list[range]) -> list[int]:
     invalid_ids: list[int] = []
+
+    for rangeset in ranges:
+        for id_ in rangeset:
+            if not is_valid_id(id_):
+                invalid_ids.append(id_)
+                LOGGER.debug("Found invalid id: %d", id_)
+
     return invalid_ids
 
 
 def get_sum_of_invalid_ids() -> int:
     with open("data.txt", "r") as fp:
         ranges = get_ranges(fp.read())
+        LOGGER.debug("ranges: %s", ranges)
         invalid_ids = check_ids(ranges)
+        LOGGER.debug("invalid ids: %s", invalid_ids)
         return sum(invalid_ids)
 
 
